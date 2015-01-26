85ac1509dcfad32e9673a39aeeaaadd2fe0a7864
Harm Jan Luth <harmjan.luth@marviq.com>
2015-01-26 16:32:37 +0100

    Added extra filters for search

--- build/datastore/models/location/index.js
+++ build/datastore/models/location/index.js
@@ -11,10 +11,27 @@ utils = roquire("utils");
 LocationModel = mongoose.model("Location", schemas.location);
 
 exports.find = function(query, callback) {
+  var filters;
+  query.features = ["Bereikbaar per boot", "Invalidentoegankelijk", "Tuin"];
+  filters = {};
+  if (query.pax) {
+    filters.maximum_pax = {
+      $gte: query.pax
+    };
+    filters.minimum_pax = {
+      $lte: query.pax
+    };
+  }
+  if (query.features) {
+    filters.features = {
+      $all: query.features
+    };
+  }
   return LocationModel.geoNear(query.latlng, {
     distanceMultiplier: 6371,
-    maxDistance: query.distance / 637.1,
-    spherical: true
+    maxDistance: query.distance / 6371,
+    spherical: true,
+    query: filters
   }, function(err, results, stats) {
     return callback(results);
   });
--- src/datastore/models/location/index.coffee
+++ src/datastore/models/location/index.coffee
@@ -9,10 +9,27 @@ LocationModel 		= mongoose.model "Location", schemas.location
 # Module
 exports.find = ( query, callback ) ->
 
+	query.features = [ "Bereikbaar per boot", "Invalidentoegankelijk", "Tuin" ]
+
+	# New filters
+	filters 					= {}
+
+	# Set pax
+	if query.pax
+		filters.maximum_pax 	= $gte : query.pax
+		filters.minimum_pax 	= $lte : query.pax
+
+	if query.features
+		filters.features		= $all : query.features
+
+	# Execute query
 	LocationModel.geoNear query.latlng,
 		distanceMultiplier	: 6371
-		maxDistance 		: query.distance / 637.1
+		maxDistance 		: query.distance / 6371
 		spherical			: true
+		,
+		query :
+			filters
 		, (err, results, stats) ->
 			
 			callback( results )
